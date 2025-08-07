f2af7de5ea3db3dc4826897aff1963779f698c87
FEBasata <erenbasata@gmail.com>
2025-08-07 21:32:07 +0300

    Temel server bağlantısı kuruldu, gereksiz klasör kaldırıldı

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -1,15 +1,67 @@
+import 'dart:js/js_wasm.dart'; // --> Bu ne için Mert ?
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter/material.dart';
 import 'screens/acm/acm_popup.dart'; 
 
-void main() => runApp(MaterialApp(home: BubbleMenuDemo()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Asenkron işlemleri için
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+  await Supabase.initialize(
+    url: //"Supabase URL",
+    anonKey: //"Supabase anonkey",
+  );
+  runApp(const MyApp());
+}
+
+// test
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Hocam Connect",
       debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _future = Supabase.instance.client
+      .from("users")
+      .select();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder(
+        future: _future,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          final items = snapshot.data!;
+          return ListView.builder(
+            itemCount: items.length,
+            itemBuilder: ((context, index) {
+              final item = items[index];
+              return ListTile(
+                title: Text(item['name']),
+              );
+            }),
+          );
+        },
+      ),
     );
   }
 }
--- subscribtion/android/app/src/main/kotlin/com/example/subscribtion/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.subscribtion
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
